d73d4d03de8075631e13f4648d6af8e16cd4d0a5
hiclassic <hares.ahmed.319@gmail.com>
2025-07-27 02:02:36 +0600

    delete table info

--- index.php
+++ index.php
@@ -210,5 +210,24 @@ if ($page == 'insert') {
 }
 ?>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+function deleteRecord(id) {
+  Swal.fire({
+    title: 'Are you sure?',
+    text: 'This will delete the product!',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Yes, delete it!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      window.location.href = '?delete_id=' + id + '&page=view';
+    }
+  });
+}
+</script>
+</body>
+</html>
+<?php $conn->close(); ?>
 
 
