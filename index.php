c8d442c4af42f95c18c8ddde4d2be40f08eedeed
hiclassic <hares.ahmed.319@gmail.com>
2025-07-27 01:50:25 +0600

    insert product

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if ($conn->connect_error) {
 }
 
 
-// ✅ Insert Manufacturer
+// Insert Manufacturer
 if (isset($_POST['submit_manufacturer'])) {
   $name = $_POST['name'];
   $address = $_POST['address'];
@@ -20,3 +20,21 @@ if (isset($_POST['submit_manufacturer'])) {
     echo "Error: " . $conn->error;
   }
 }
+
+
+// Insert Product
+if (isset($_POST['submit_product'])) {
+  $pname = $_POST['product_name'];
+  $pprice = $_POST['product_price'];
+  $mid = $_POST['manufacturer_id'];
+  $sql = "INSERT INTO product (name, price, manufacturer_id) VALUES ('$pname', '$pprice', '$mid')";
+  if ($conn->query($sql) === TRUE) {
+    echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
+    <script>Swal.fire({icon:'success',title:'Added!',text:'Product Added!'}).then(() => { window.location.href='?page=view'; });</script>";
+    exit;
+  } else {
+    echo "Error: " . $conn->error;
+  }
+}
+
+
